b4807c18f01a766e14f64675947a45e2e1b67724
yewchong2207 <75685972+yewchong2207@users.noreply.github.com>
2020-12-31 18:05:11 +0800

    Create delete.php

---
+++ CRUD_Assets/delete.php
@@ -0,0 +1,24 @@
+<?php
+
+include ('db_connect.php');
+
+if(isset($_GET['assetID']))
+{
+    $assetID = $_GET['assetID'];
+}
+
+$sql =  "DELETE FROM assets WHERE assetID=$assetID"
+$result = mysqli_query($conn, $sql);
+
+if($result)
+{
+    header ("Location: main.php")
+}
+else
+{
+    echo $conn->error;
+}
+
+mysqli_close($conn);
+
+?>
