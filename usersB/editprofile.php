062049da6a425f84310c685030d6469f4a0cdce4
hamjingyi <75980087+hamjingyi@users.noreply.github.com>
2021-01-14 19:36:53 +0800

    Add files via upload

--- usersB/editprofile.php
+++ usersB/editprofile.php
@@ -1,6 +1,16 @@
 <?php 
     include("../dbconfig.php");
 
+    if(!session_id())//if session_id is not found
+    {
+        session_start();
+    }
+    
+    if(isset($_SESSION['ic']) != session_id() )
+    {
+        header('location: ../login/login.php');
+    }
+
     if(isset($_GET['id']))
     {
         $id = $_GET['id'];
@@ -13,38 +23,47 @@
           echo "ERROR:  $conn->error";
           exit();
         } 
+
         $row = mysqli_fetch_array($result);
+
+    include("editprofileprocess.php");
+    include("../navbar/navbarB1.php");
     
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+<style>
+.error {color: #FF0000;}
+.help-block{color:red;}
+</style>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Profile</title>
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </head>
 <body>
-    <div class="container">
+   <form action="" method="POST">
+        <div class="container-fluid">
+    <h3 class="text-dark mb-4" style="font-size: 40px;">User Profile</h3>
+    <div class="col">
+        <div class="card shadow mb-3">
+            <div class="card-header py-3">
+                <p class="text-primary m-0 font-weight-bold">User Settings</p>
+            </div>
+            <div class="card-body">
+                <div class ="row">
+                    <div class="col-6">
+                        <label for="fname" class="form-label">Full Name:</label>
+                        <input type="text" name="name" id="fname" class="form-control" placeholder="Enter Full Name" value="<?php echo $row["name"]; ?>" readonly>
+                    </div>
 
-        <form action="editprofileprocess.php" method="POST">
-          <h2>User Profile: </h2>
-          <div class ="row">
+                    <div class="col-6">
+                        <label for="fic" class="form-label <?php echo (!empty($ICErr)) ? 'is-invalid' : ''; ?>">IC number:</label>
+                        <input type="text" name="IC" id="fic" class="form-control" placeholder="Enter IC Number"value="<?php echo $row["u_userIC"]; ?>"readonly>
+                    </div>
+                </div><br>    
 
-              <div class="col-6">
-                  <label for="fname" class="form-label">Full Name:</label>
-                  <input type="text" name="name" id="fname" class="form-control form-control" placeholder="Enter Full Name" value="<?php echo $row["name"]; ?>" readonly/>
-              </div>
-
-              <div class="col-6">
-                  <label for="fic" class="form-label">IC number:</label>
-                  <input type="text" name="IC" id="fic" class="form-control form-control" placeholder="Enter IC Number" value="<?php echo $row["u_userIC"]; ?>" readonly/>
-              </div>
-
-          </div><br>    
           <script>
               var check = function() {
                 if (document.getElementById('password').value ==
@@ -57,42 +76,59 @@
                 }
               }
           </script>
+
           <div class ="row">
-              <div class="col-6">
-                  <label for="pwd" class="form-label">Password:</label>
-                  <input type="password" name="password" id="password" class="form-control form-control" placeholder="Enter password" value="<?php echo $row["pwd"]; ?>" required>
+              <div class="col-6 needs-validation">
+                  <label for="pwd" class="form-label ">Password:</label>
+                  <input type="password" name="password" id="password" class="form-control <?php echo (!empty($passwordErr)) ? 'is-invalid' : ''; ?>" title="Enter at least 4 characters." placeholder="Enter password" value="<?php echo $row["pwd"]; ?>" >
+                  <span class="help-block"><?php echo $passwordErr;?></span>
               </div>
 
               <div class="col-6">
                   <label for="psw-repeat" class="form-label">Re-type Password:<span id='message'></span></label>  
-                <input type="password" id="confirm_password" placeholder="Retype Password" name="confirm_password" class="form-control form-control" onkeyup='check();' value="<?php echo $row["pwd"]; ?>" required/>  
+                <input type="password" id="confirm_password" placeholder="Retype Password" name="confirm_password" class="form-control  <?php echo (!empty($confirm_passwordErr)) ? 'is-invalid' : ''; ?>" onkeyup='check();'value="<?php echo $row["pwd"]; ?>">
+                <span class="help-block"><?php echo $confirm_passwordErr;?></span>  
               </div>
           </div><br>
+          <label for="upload" class="form-label">Upload Profile Picture:</label><br>
+          <input type="file" name="image" id="fileToUpload">
+  </div>
+</div> 
+        <div class="card shadow">
+                  <div class="card-header py-3">
+                      <p class="text-primary m-0 font-weight-bold">Contact Settings</p>
+                  </div>
+                  <div class="card-body">
 
-          <div class="mb-3">
+            <div class="mb-3">
               <label for="faddr" class="form-label">Home Address:</label>
-              <input type="text" name="faddr" id="faddr" class="form-control form-control" placeholder="Enter Home Address" value="<?php echo $row["address"]; ?>"required/>
-          </div>
+              <input type="text" name="faddr" id="faddr" class="form-control <?php echo (!empty($addrErr)) ? 'is-invalid' : ''; ?>" placeholder="Enter Home Address" value="<?php echo $row["address"]; ?>">
+              <span class="help-block"><?php echo $addrErr;?></span>
+            </div>
 
-          <div class="row">
-            <div class="col-6">
-              <label for="fcontactnum" class="form-label">Contact Number:</label>
-              <input type="text" name="fcontactnum" id="fcontactnum" class="form-control form-control" placeholder="Enter contact number" value="<?php echo $row["contact"]; ?>" required/>
+             <div class="row">
+                <div class="col-6">
+                  <label for="fcontactnum" class="form-label">Contact Number:</label>
+                  <input type="text" name="fcontactnum" id="fcontactnum" class="form-control <?php echo (!empty($contactErr)) ? 'is-invalid' : ''; ?>" placeholder="Enter contact number"  value="<?php echo $row["contact"]; ?>">
+                  <span class="help-block"><?php echo $contactErr;?></span>
+                </div>
+     
+                <div class="col-6">
+                  <label for="femail" class="form-label">Email Address:</label>
+                  <input type="text" name="femail" id="femail" class="form-control <?php echo (!empty($emailErr)) ? 'is-invalid' : ''; ?>" placeholder="Enter email address"value="<?php echo $row["email"]; ?>">
+                  <span class="help-block"><?php echo $emailErr;?></span>
+                </div>
             </div>
-            
-            <div class="col-6">
-              <label for="femail" class="form-label">Email Address:</label>
-              <input type="text" name="femail" id="femail" class="form-control form-control" placeholder="Enter email address"value="<?php echo $row["email"]; ?>">
-            </div>
-          </div><br>
-              
-          <input type="submit" name="submit" class="btn btn-primary" value="Save"/>
-            
+ </div>
+</div><br>
+          <input type="submit" text-align:center name="submit" class="btn btn-primary" value="Save"/>
+</div>     
+          
         </form>
     </div>
+
 </body>
 </html>
 
-<?php
-    }
-?>
+<?php include("../navbar/navbar2.php"); } ?>
+
--- usersB/editprofileprocess.php
+++ usersB/editprofileprocess.php
@@ -1,39 +1,126 @@
 <?php
-    if(!session_id())//if session_id is not found
-    {
-        session_start();
-    }
+    // if(!session_id())//if session_id is not found
+    // {
+    //     session_start();
+    // }
     
-    if(isset($_SESSION['ic']) != session_id() )
+    // if(isset($_SESSION['ic']) != session_id() )
+    // {
+    //     header('location: ../login/login.php');
+    // }
+    // include("../dbconfig.php");
+
+    $password = "";
+    $confirm_password = "";
+    $addr = "";
+    $contact = "";
+    $email= "";
+
+    $passwordErr = "";
+    $confirm_passwordErr = "";
+    $addrErr = "";
+    $contactErr = "";
+    $emailErr= "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") 
     {
-        header('location: ../login/login.php');
-    }
-    include("../dbconfig.php");
 
-    $IC = $_POST['IC'];
-    $password = $_POST['password'];
-    $addr = $_POST['faddr'];
-    $contact = $_POST['fcontactnum'];
-    $email= $_POST['femail'];
+      if(empty(trim($_POST["password"])))
+      {
+          $passwordErr = "Changes failed. Pasword is required.";     
+      } 
+      elseif(strlen(trim($_POST["password"])) < 4)
+      {
+          $passwordErr = "Changes failed. Password must have at least 4 characters.";
+      } 
+      else
+      {
+          $password = trim($_POST["password"]);
+      }
 
-    $sql = "UPDATE users
+
+
+      if(empty(trim($_POST["confirm_password"])))
+      {
+          $confirm_passwordErr = "Changes failed. Please confirm password.";     
+      } 
+      elseif(strlen(trim($_POST["confirm_password"])) < 4)
+      {
+          $confirm_passwordErr = "Changes failed. Password must have at least 4 characters.";
+      } 
+      elseif($_POST["password"]!=$_POST["confirm_password"])
+      {
+           $confirm_passwordErr = "Changes failed. Password is not matching with the left field.";
+      }
+      else
+      {
+          $confirm_password = trim($_POST["confirm_password"]);
+      }
+
+
+      if (empty(trim(($_POST["faddr"])))) 
+      {
+        $addrErr = "Changes failed. Address is required";
+      } 
+      else
+      {
+        $addr = trim($_POST["faddr"]);
+      }
+
+
+      if (empty($_POST["fcontactnum"])) 
+      {
+        $contactErr = "Changes failed. Contact number is required";
+      } 
+      elseif (!preg_match('/^[0-9]{10,11}$/',$_POST["fcontactnum"])) 
+      {
+        $contactErr = "Changes failed. Please enter correct format in digit without - and without +60 ";
+      }
+      else
+      {
+        $contact = trim($_POST["fcontactnum"]);
+      }
+
+
+      if (empty(trim(($_POST["femail"])))) 
+      {
+        $emailErr = "Changes failed. Email is required";
+      } 
+      elseif (!filter_var($_POST["femail"], FILTER_VALIDATE_EMAIL)) 
+      {
+        $emailErr = "Changes failed. Invalid email format";
+      }
+      else
+      {
+        $email = trim($_POST["femail"]);
+      }
+
+      if(empty($emailErr)&&empty($contactErr)&&empty($passwordErr)
+              &&empty($confirm_passwordErr)&&empty($addrErr))
+      {
+        $sql = "UPDATE users
             SET  pwd='".$password."',address='".$addr."', email='".$email."', contact= '".$contact."'
-            WHERE u_userIC=".$IC."";
+            WHERE u_userIC=".$id."";
 
-    echo '\n';
-    echo $sql;
-    echo '\n';
-    
-    $result = mysqli_query($conn, $sql);
+        echo '\n';
+        echo $sql;
+        echo '\n';
+        
+        $result = mysqli_query($conn, $sql);
 
-    if($result)
-    {
-        header("location: landing.php?id=".$IC."");
-    } 
-    else
-    {
-        echo "ERROR: $conn->error";
-    }
 
-    mysqli_close($conn);
+        if($result)
+        {
+            header("location: ../indexB.php");
+        } 
+        else
+        {
+            echo "ERROR: $conn->error";
+        }
+
+        mysqli_close($conn);
+
+      }
+}
+
 ?>
