f6cf621ab0f280c9f406d6a437398520465e4dfe
hamjingyi <75980087+hamjingyi@users.noreply.github.com>
2021-01-14 11:06:48 +0800

    Add files via upload

--- navbar/navbar2.php
+++ navbar/navbar2.php
@@ -1,24 +1,10 @@
-<?php { ?>
-<!DOCTYPE html>
-<html>
-<head>
-	
-</head>
-<body>
-
-</body>
-</html>
-       </div>
-        </div>
+		</div>
     </div>
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</div>
+    <script src="../assets/js/jquery.min.js"></script>
+    <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
-    <script src="assets/js/script.min.js"></script>
+    <script src="../assets/js/script.min.js"></script>
 
 </body>
 </html>
-
-<?php
-}
-?>
--- navbar/navbarB1.php
+++ navbar/navbarB1.php
@@ -1,20 +1,5 @@
-<?php
-    
-    $sql1 = "SELECT * FROM users WHERE u_userIC=".$_SESSION['ic'].";";
-
-    $result1 = mysqli_query($conn, $sql1);
-
-    if (!$result1)
-    {
-        echo "ERROR:  $conn->error";
-        header("refresh: 6; location: readUser.php");
-    } 
-
-    $row1 = mysqli_fetch_array($result1);
-?>
-
 <!DOCTYPE html>
-<html  lang="en" style="opacity: 1;">
+<html lang="en" style="opacity: 1;">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
@@ -25,50 +10,44 @@
     <link rel="stylesheet" href="../assets/fonts/font-awesome.min.css">
     <link rel="stylesheet" href="../assets/fonts/ionicons.min.css">
     <link rel="stylesheet" href="../assets/fonts/fontawesome5-overrides.min.css">
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script> -->
-
 </head>
 
 <body id="page-top">
     <div id="wrapper">
         <nav class="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0" style="opacity: 1;filter: brightness(94%) contrast(114%);background: #0902d6;color: rgb(255,255,255);">
-            <div class="container-fluid d-flex flex-column p-0"><a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="../index.php">
+            <div class="container-fluid d-flex flex-column p-0"><a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="../index.php?id=<?php echo $row["u_userIC"]; ?>">
                     <div class="sidebar-brand-icon rotate-n-15"></div>
                     <div class="sidebar-brand-text mx-3"><span>Management</span></div>
                 </a>
 
                 <hr class="sidebar-divider my-0">
                 <ul class="nav navbar-nav text-light" id="accordionSidebar">
-                    <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-tachometer-alt"></i><span>Dashboard</span></a><a class="nav-link" href="../complaints/readComplaint.php"><i class="fas fa-user-circle"></i><span>Complaint Management</span></a></li>
-                    <li class="nav-item"><a class="nav-link" href="../users/readUser.php"><i class="fas fa-user"></i><span>User Management</span></a></li>
-                    <li class="nav-item"><a class="nav-link" href="../assets/main.php"><i class="fas fa-table"></i><span>Asset Managemnt</span></a></li>
-                    <li class="nav-item"><a class="nav-link" href="../Rooms/roomlist.php"><i class="far fa-user-circle"></i><span>Room Management</span></a><a class="nav-link" href="../Blocks/blocklist.php"><i class="fa fa-building"></i><span>Block Management</span></a><a class="nav-link" href="#"><i class="fa fa-files-o"></i><span>Download And Print</span></a></li>
-                    <li class="nav-item"></li>
+                    <li class="nav-item"></a><a class="nav-link" href="../complaints/readComplaint.php"><i class="fas fa-user-circle"></i><span>Complaint Management</span></a></li>
+                    <li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-files-o"></i><span>Download And Print</span></a></li>
                 </ul>
                 <div class="text-center d-none d-md-inline"><button class="btn rounded-circle border-0" id="sidebarToggle" type="button"></button></div>
+
+                <script src="../assets/js/jquery.min.js"></script>
+                <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
+                <script src="../assets/js/script.min.js"></script>
+
             </div>
-
-            <!-- <script src="../assets/js/jquery.min.js"></script>
-            <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
-            <script src="../assets/js/script.min.js"></script> -->
         </nav>
-
         <div class="d-flex flex-column" id="content-wrapper">
             <div id="content" style="margin: 2px;">
                 <nav class="navbar navbar-light navbar-expand bg-white shadow mb-4 topbar static-top">
                     <div class="container-fluid"><button class="btn btn-link d-md-none rounded-circle mr-3" id="sidebarToggleTop" type="button"><i class="fas fa-bars"></i></button>
+                        <h3 class="text-dark mb-0"><?php echo $row["postBI"];?> Home</h3>
                         <ul class="nav navbar-nav flex-nowrap ml-auto">
                             
                             <div class="d-none d-sm-block topbar-divider"></div>
                             <li class="nav-item dropdown no-arrow">
                                 <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#"><span class="d-none d-lg-inline mr-2">
-                                    <?php echo $row1["name"];?>
-                                    
+                                    <?php echo $row["name"];?>
                                 </span><img class="border rounded-circle img-profile" src="../assets/img/avatars/avatar5.png">&nbsp;&nbsp;<i class="icon ion-android-settings"></i></a>
                                     <div class="dropdown-menu shadow dropdown-menu-right animated--grow-in">
-                                        <a class="dropdown-item" href="../users/updateUser.php?id=<?php echo $row1["u_userIC"]; ?>"><i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>&nbsp;Edit Profile</a>
+                                        <a class="dropdown-item" href="../usersB/editprofile.php?id=<?php echo $row["u_userIC"]; ?>"><i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>&nbsp;Edit Profile</a>
                                         <a class="dropdown-item" href="#"><i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>&nbsp;Settings</a>
                                     <div class="dropdown-divider"></div>
                                         <a class="dropdown-item" href="../login/logout.php"><i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>&nbsp;Logout</a>
@@ -78,10 +57,3 @@
                         </ul>
                     </div>
                 </nav>
-
-
-
-            
-
-
-
