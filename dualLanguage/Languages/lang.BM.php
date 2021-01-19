3c22f8ae20c0567e553d93315c9380f2a8ba6677
hamjingyi <75980087+hamjingyi@users.noreply.github.com>
2021-01-19 22:53:54 +0800

    Update lang.BM.php

--- dualLanguage/Languages/lang.BM.php
+++ dualLanguage/Languages/lang.BM.php
@@ -22,33 +22,33 @@ $language['pie2'] = 'CARTA ADUAN UNTUK ASET ALIH/ASET BUKAN ICT';
 $language['Complaint Status'] = 'Status Aduan';
 $language['KVPJB Complaint Inventory System'] = 'Sistem Inventori Aduan KVPJB'; 
 
+
 //$language[''] = '';
-
-//navbar1
-$language['Admin Home'] = '';
-$language['Admin Main Page'] = '';
-$language['ADMIN'] = '';
-
-$language['Edit Profile'] = 'Kemaskini Profil';
-$language['Change Password'] = 'Tukar Kata Laluan';
-$language['Logout'] = 'Log Keluar';
-$language['MANAGEMENT'] = 'LAMAN UTAMA';
-$language['MY DASHBOARD'] = 'PAPARAN SISTEM';
-$language['COMPLAINTS MANAGEMENT'] = 'ADUAN';
-$language['USER MANAGEMENT'] = 'PENGGUNA';
-$language['ROOM MANAGEMENT'] = 'BILIK';
-$language['BLOCK MANAGEMENT'] = 'BLOK';
-$language['ASSET MANAGEMENT'] = 'ASET';
-$language['DOWNLOAD AND PRINT'] = 'MUAT TURUN DAN CETAK DOCUMENT';
-$language['GRADE MANAGEMENT'] = 'GRED';
+$language['MY DASHBOARD'] = 'LAMAN UTAMA';
+$language['DASHBOARD'] = 'LAMAN UTAMA';
+$language['COMPLAINT MANAGEMENT'] = 'PENYELENGGARAAN ADUAN';
+$language['USER MANAGEMENT'] = 'PENYELENGGARAAN PENGUNA';
+$language['ROOM MANAGEMENT'] = 'PENYELENGGARAAN BILIK';
+$language['ASSET MANAGEMENT'] = 'PENYELENGGARAAN ASET';
+$language['BLOCK MANAGEMENT'] = 'PENYELENGGARAAN BLOK';
+$language['DOWNLOAD AND PRINT'] = 'MUAT TURUN DAN CETAK';
+$language['GRADE MANAGEMENT'] = 'PENYELENGGARAAN GRED';
+$language['no picture avaiable'] = 'tiada gambar yang tersedia';
 
 //CRUD
 $language['Action'] = 'Tindakan';
 $language['Create'] = 'Tambah';
 $language['View'] = 'Info';
-$language['Edit'] = 'Mengedit';
+$language['Edit'] = 'Kemaskini';
 $language['Delete'] = 'Padam';
 $language['Cancel'] = 'Batal';
+$language['Clear'] = 'Kosongkan';
+$language['Submit'] = 'Hantar';
+$language['Save'] = 'Simpan';
+$language['Reset'] = 'Tetapkan Semula';
+$language['Search'] = 'Cari';
+$language['Apply'] = 'Hantar';
+$language['Accept'] = 'Terima'; 
 
 
 //Complaints
@@ -65,28 +65,86 @@ $language['Location'] = 'Lokasi';
 $language['Rooms'] = 'Bilik';
 $language['Rooms in this block'] = 'Bilik dalam blick ini';
 $language['PIC'] = 'PIC';
+$language['Block Name(English)'] = 'Nama Bilik (Bahasa Inggeris)';
+$language['Block Name(Malay)'] = 'Nama Bilik (Bahasa Melayu)';
+$language["BLOCKS' LIST"] = "SENARAI BLOK";
+$language['New Block'] = 'Blok Baru';
+$language['Hostel'] = 'Asrama';
+$language['College'] = 'Kolej';
+$language['Others'] = 'Lain-lain';
+$language['New Room']  = 'Bilik Baru';
+$language['Rooms in this block'] = 'Bilik Dalam Blok';
 
+
+    //create
+$language['Block ID'] = 'Blok ID';
+$language['title'] = 'DAFTAR BLOK BARU';
+$language["BLOCK'S DETAILS"] = "PERINCIAN BLOK";
+
+  //modify
+$language['Edit Block'] = 'Kemaskini Blok';
+$language['EDIT BLOCK'] = 'KEMASKINI BLOK';
+
+    //blockroom
+$language['Rooms in'] = 'Bilik Dalam ';
+$language["Room's Name(English)"] = "Nama Bilik(Bahasa Inggeris)";
+$language["Room's Name(Malay)"] = "Nama Bilik(Bahasa English)";
+
+
+//room
+$language["ROOMS' LIST"] = "SENARAI BILIK";
+$language['Room ID'] = "Bilik ID";
+$language['Assets'] = 'Aset';
+$language["Person in charge's ID"] = "PIC ID";
+$language['Rooms in KVPJB'] = 'Bilik Dalam KVPJB';
+$language['Update Room'] = 'Kemaskini Maklumat Bilik';
+$language['Edit Room'] = 'Kemaskini Maklumat Bilik';
+
+    //create
+$language['CREATE A NEW ROOM'] = 'DAFTAR BILIK BARU';
+$language["ROOM'S DETAILS"] = "PERINCIAN BILIK";
+$language["Room's Image"] = "Gambar Bilik";
+$language['CLICK HERE'] = 'TEKAN SINI';
+
+  //modify
+$language['Edit Room'] = 'Kemaskini Maklumat Bilik';
+$language['EDIT ROOM'] = 'KEMASKINI MAKLUMAT BILIK';
+
+    //roomdetail
+$language["Room's Information"] = "MAKLUMAT BILIK";
+$language["Room's Name"] = "Nama Bilik";
+$language['Located in'] = 'Bertempat di';
+    //roomassets
+$language['Assets in'] = 'Aset Dalam ';
+//Block 
+
+$language['Blocks in KVPJB'] = 'Blok Dalam KVPJB';
+$language['Update_Block'] = 'Kemaskini Maklumat Blok';
 
 
 
 //Login Form
+$language['Login Page'] = 'Halaman Log Masuk';
 $language['Login Form'] = 'Log Masuk';
 $language['Username:'] = 'Nama Penguna:';
 $language['Password:'] = 'Kata Laluan:';
 $language['Login'] = 'Log Masuk';
 $language['Remember me'] = 'Ingat saya';
 $language['Forget Password?'] = 'Lupa Kata Laluan?';
+$language['Filter'] = 'Tapisan';
 
 // Users
 $language['User Information'] = 'Maklumat Penguna';
 $language['Full Name'] = 'Nama Penuh';
 $language['Position'] = 'Jawatan';
-$language['Grade'] = 'Grad';
-$language['User IC'] = 'No IC Penguna';
+$language['Grade'] = 'Gred';
+$language['User IC'] = 'Nombor IC';
 $language['Number Of Complaints'] = 'Bilangan Aduan';
-$language['Contact'] = 'Hubungi';
+$language['Contact'] = 'Nombor Telefon';
 $language['User List'] = 'Senarai Penguna';
 
+//Login Process
+$language['Invalid Login'] = 'Log masuk tidak sah';
 
 //Assets
 $language['Asset ID'] = 'No siri Aset';
@@ -94,7 +152,7 @@ $language['Name'] = 'Nama Aset';
 $language['Category'] = 'Kategori';
 $language['Room ID'] = 'No siri bilik';
 $language['Description'] = 'Penerangan';
-$language['Cost'] = 'Kos';
+$language['Cost'] = 'Harga';
 $language['Amount'] = 'Bilangan';
 $language['Date purchased'] = 'Tarikh dibeli';
 $language['Manage Asset'] = 'Pengendalian Aset';
@@ -102,6 +160,299 @@ $language['ICT'] = 'ICT';
 $language['Non-ICT'] = 'Bukan ICT';
 $language['Create Asset'] = 'Tambah Aset';
 
+$language['BI Name'] = 'Nama aset (Bahasa Inggeris)';
+$language['BM Name'] = 'Nama aset (Bahasa Melayu)';
+$language['Description'] = 'Penerangan';
+$language['Are you sure you want to delete this assets?'] = 'Adakah anda pasti mahu memadamkan aset ini?';
+$language['Asset Information'] = 'Maklumat Aset';
+$language['no picture avaiable'] = 'gambar tidak ada';
+$language['Select a block before viewing the assets of that block'] = 'Pilih blok sebelum memilih aset blok itu';
+
+
+//Process
+//Forms 
+$language['Asset Main_Page'] = 'Halaman Utama Aset';
+$language['Update Asset'] = 'Kemaskini Maklumat Aset';
+
+
+
+//Complaint
+$language['No'] = 'Tiada';
+$language['ZirconTech'] = 'ZirconTech';
+$language['Complaint Form'] = 'Borang Aduan';
+$language['Date:'] = 'Tarikh:';
+$language['Blocks'] = 'Bloks';
+$language['Detail:'] = 'Perincian:';
+$language['Complaints ID:'] = 'No Siri Aduan:';
+$language['Building:'] = 'Bangunan:';
+$language['Room:'] = 'Bilik:';
+
+$language['Proposed Date:'] = 'Tarikh Diadu:';
+$language['Settled Date:'] = 'Tarikh Penyelesaian:';
+
+$language['Open this select menu'] = 'Bukan pilihan ini';
+$language['Pending Complaints'] = 'Aduan Yang Belum Selesai';
+$language['View Accepted Complaints'] = 'Semak aduan yang diterima';
+$language['search by proposer or follower name'] = 'Cari';
+$language['Accepted Complaints List'] = 'Aduan Yang Diterima';
+$language['All Complaints'] = 'Senarai Aduan Yang Lengkap';
+//landing  
+
+$language['Landing Page'] = 'Halaman Utama';
+
+//Login and logout
+$language['Reset Password'] = 'Tetapkan Semula Kata Laluan';
+$language['Full Name:'] = 'Nama Penuh:';
+
+$language['IC number:'] = 'Nombor IC:';
+$language['Password:'] = 'Kata Laluan:';
+$language['Re-type Password:'] = 'Masuk Semula Kata Laluan:';
+$language['Check IC'] = 'Semak IC';
+$language['Username'] = 'Nama Pengguna';
+$language['Check'] = 'Semak';
+$language['Complaints Inventory System'] = 'Sistem Penyelenggaraan Aduan';
+
+$language['Back To Login Page'] = 'Kembali Ke Halaman Log Masuk';
+
+//navbar1
+$language['Admin Home'] = 'Halaman Pentadbir';
+$language['Admin Main Page'] = 'Halaman Utama Pentadbir';
+$language['ADMIN'] = 'PENTADBIR';
+
+$language['Edit Profile'] = 'Kemaskini Profil';
+$language['Logout'] = 'Logout';
+
+//Profile
+$language['User Profile:'] = 'Maklumat Pengguna:';
+$language['Email Address:'] = 'Emel:';
+$language['Landing For Other Users'] = 'Halaman Pengguna Lain';
+$language['View Profile'] = 'Semak Profil';
+
+//users
+$language['User_Details'] = 'Perincian Pengguna';
+$language['Name:'] = 'Nama:';
+$language['IC:'] = 'IC:';
+$language['Position:'] = 'Jawatan:';
+$language['Contact:'] = 'Nombor Telefon:';
+$language['Registered Date:'] = 'Tarikh Pendaftaran:';
+$language['Ending Date:'] = 'Tarikh Pengakhiran:';
+
+//landing 
+$language['Landing For Admin'] = 'Halaman Pentadbir';
+$language['ReadList'] = 'Senarai';
+$language['Users List'] = 'Senarai Pengguna';
+$language['Update User'] = 'Kemaskini Maklumat Pengguna';
+$language['User Information:'] = 'Maklumat Pengguna:';
+$language['IC number:'] = 'Nombor IC:';
+$language['Position Assigned:'] = 'Jawatan:';
+$language['Choose a position'] = 'Pilih jawatan';
+$language['Room Managed:'] = 'Bilik Yang Diurus:';
+$language['Upload Profile Picture:'] = 'Muat Naik Gambar Profil:';
+
+
+//createUser 
+$language['Create_Account'] = 'Daftar Akaun Baru';
+
+$language['Password must contain the following:'] = 'Kata laluan mesti mengandungi yang berikut:';
+$language['A'] = 'A';
+
+$language['Minimum'] = 'Minimum';
+$language['8 characters'] = '8 aksara';
+$language['capital (uppercase)'] = 'Huruf Besar';
+$language['letter'] = 'abjab';
+$language['number'] = 'nombor';
+
+
+
+
+//deleteuser
+$language['ERROR_Occur!ERROR Occur! Will direct back to the same page in 5 seconds'] = 'Kesilapan Berlaku! Akan mengarahkan kembali ke halaman yang sama dalam 5 saat';
+
+
+//userB or validation 
+$language['Choose a location.'] = 'Pilih Lokasi.';
+$language['Malay block name is required'] = 'Blok Nama dalam Bahasa Melayu diperlukan.';
+$language['Choose a name.'] = 'Pilih nama.';
+$language['Choose a block.'] = 'Pilih blok.';
+$language['Asset ID is required'] = 'Nombor siri aset diperlukan.';
+$language['English asset name is required'] = 'Aset Nama dalam Bahasa Inggeris diperlukan.';
+$language['Malay asset name is required'] = 'Aset Nama dalam Bahasa Melayu diperlukan.';
+$language['Choose a category.'] = 'Pilih kategori.';
+$language['Cost is required'] = 'Harga diperlukan.';
+$language['Please enter valid cost.'] = 'Sila masukkan nombor yang sah.';
+$language['Amount is required'] = 'Bilangan diperlukan.';
+$language['Please enter valid amount.'] = 'Sila masukkan nombor yang sah.';
+$language['IC number is required'] = 'Nombor IC diperlukan.';
+$language['Please_enter_12_digit_without_-'] = 'Sila masukkan 12 digit tanpa -';
+$language['Choose a date.'] = 'Pilih Tarikh.';
+$language['Choose a block.'] = 'Pilih Blok.';
+$language['Choose a room.'] = 'Pilih bilik.';
+$language['Choose an asset.'] = 'Pilih aset.';
+$language['Detail is required.'] = 'Perincian diperlukan.';
+
+
+// Dashboard 
+$language['NumberComp'] = 'Bilangan Aduan';
+$language['NumberCompSolved'] = ' Bilangan Aduan Yang Selesai';
+$language['NumberCompPend'] = 'Bilangan Aduan Yang Belum Selesai';
+$language['num_User'] = 'bilangan pengguna';
+$language['number'] = 'nombor';
+$language["Assets's name"] = "Nama aset";
+$language['Number of Complaint'] = 'Bilangan aduan';
+$language["Blocks' name"] = "Nama bilik";
+$language['Number of Complaint'] = 'Bilangan aduan';
+
+
+
+
+//YewChong
+$language["Need"] = "Perlu";
+$language["No Need"] = "Tidak Perlu";
+$language['Open menu'] = 'Buka pilihan';
+$language['Asset Category'] = 'Kategori Aset';
+$language['All Complaints List'] ='Senarai Aduan Yang Lengkap' ;
+$language['New Complaint'] = 'Aduan Baru';
+$language['Assets'] = 'Aset';
+$language['Proposed By'] = 'Diadu oleh';
+$language['Issue Date'] = 'Tarikh Diadu';
+$language['Settled Date'] = 'Tarikh Penyelesaian';
+$language['Accepted by'] = 'Diterima oleh';
+$language['Choose A Block'] = 'Pilih blok';
+$language['Choose A Room'] ='Pilih bilik';
+$language['Choose A Category'] = 'Pilih kategori' ; 
+$language['Complaint Detail'] = 'Perincian aduan';
+$language['Please Choose A Room'] = 'Pilih bilik';
+$language['Complaint Image'] = 'Gambar Aduan';
+$language["Complainant's detail"] = "Perincian aduan";
+$language['ZirconTech'] = 'ZirconTech';
+$language['Zircon Tech'] = 'Zircon Tech';
+
+//detailcomplaint
+$language['Complaint ID'] = 'Nombor Siri Aduan' ;
+$language['Building'] = 'Bangunan' ; 
+$language['Room'] = 'Bilik';
+$language['Proposed Date'] = 'Tarikh Diadu';
+$language['Detail'] = 'Perincian';
+$language['Complaint Information'] = 'Maklumat Aduan';
+
+
+//Ham Jing Yi
+//Login, forget password
+$language['Show Password:'] = ' Tunjuk Kata Laluan';
+$language['Check IC'] = 'Semak IC';
+$language['Check'] = 'Semak';
+$language['Enter IC'] = 'Masukkan IC';
+$language['Enter Password'] = 'Masukkan Kata Laluan';
+$language['User is found!'] = 'Pengguna wujud!';
+$language['Reset Password:'] = 'Menetapkan Semula Kata Laluan:';
+$language['User Settings'] = 'Tetapan Pengguna';
+$language['Enter Full Name'] = 'Masukkan Nama Penuh';
+$language['New Password:'] = 'Kata Laluan Baru:';
+$language['Enter New Password'] = 'Masukkan Kata Laluan Baru';
+$language['Re-type Password'] = 'Masukkan Semula Kata Laluan';
+$language['IC Number'] = 'Nombor IC';
+
+//navbars
+$language['Home'] = 'Halaman';
+$language['Management'] = 'Penyelenggaraan';
+$language['Dashboard'] = 'Laman Utama';
+$language['Complaint Management'] = 'Penyelenggaraan Aduan';
+$language['User Management'] = 'Penyelenggaraan Pengguna';
+$language['Asset Management'] = 'Penyelenggaraan Aset';
+$language['Room Management'] = 'Penyelenggaraan Bilik';
+$language['Block Management'] = 'Penyelenggaraan Blok';
+$language['Download and Print'] = 'Muat Turun Dan Cetak';
+$language['Grade Management'] = 'Penyelenggaraan Gred';
+$language['Edit Profile'] = 'Kemaskini Profil';
+$language['Change Password'] = 'Tukar Kata Laluan';
+$language['Logout'] = 'Log Keluar';
+$language['Index'] = 'Indeks';
+
+//users
+$language['Admin'] = 'Pentadbir';
+$language['PIC Of Room'] = 'PIC Makmal';
+$language['Assistant Computer Technician'] = 'Penolong Juruteknik Komputer';
+$language['Assistant Engineer'] = 'Penolong Jurutera';
+$language['Users List'] = 'Senarai Pengguna';
+$language['Create New User'] = 'Daftar Pengguna Baru';
+$language['Filter'] = 'Tapis';
+$language['Open this select menu'] = 'Buka pilihan ini';
+$language['Apply'] = 'Hantar';
+$language['Positions'] = 'Jawatan';
+$language['Detail'] = 'Perincian';
+$language['Delete'] = 'Padam';
+$language['Are you sure you want to delete this account?'] = 'Adakah anda pasti mahu memadamkan akaun ini?';
+$language['User Detail'] = 'Perincian Pengguna';
+$language['Username/IC'] = 'Nama Pengguna/IC';
+$language['user image'] = 'Gambar Pengguna';
+$language['Grade:'] = 'Gred:';
+$language['Choose a grade'] = 'Pilih gred';
+$language['Choose a room'] = 'Pilih bilik';
+$language['Contact Settings'] = 'Tetapan Kenalan';
+$language['Contact Number:'] = 'Nombor Telefon:';
+$language['Enter contact number'] = 'Masukkan Nombor Telefon';
+$language['Home Address:'] = 'Alamat Rumah:';
+$language['Enter Home Address'] = 'Masukkan Alamat Rumah';
+$language['Enter email address'] ='Masukkan Alamat Emel';
+$language['Save'] = 'Simpan';
+$language['Clear'] = 'Kosongkan';
+
+$language['Name is required'] = 'Nama diperlukan.';
+$language['Only letters and white space allowed'] = 'Hanya huruf dan ruang putih yang dibenarkan.';
+$language['IC number is required'] = 'Nombor IC diperlukan.';
+$language['Please enter 12 digit without - '] = 'Sila masukkan 12 digit tanpa -';
+$language['Pasword is required.'] = 'Kata Laluan diperlukan.';
+$language['Password must have at least 4 characters.'] = 'Password must have at least 4 characters.';
+$language['Password is not matching with the left field.'] = 'Kata laluan mesti mengandungi sekurang-kurangnya 4 aksara.';
+$language['Please confirm password.'] = 'Sila masukkan semula kata laluan.';
+$language['Password is not matching with the left field.'] = 'Kata laluan tidak sepadan.';
+$language['Position is required.'] = 'Jawatan diperlukan.';
+$language['Grade is required.'] = 'Gred diperlukan.';
+$language['Address is required.'] = 'Alamat rumah diperlukan.';
+$language['Contact number is required.'] = 'Telefon nombor diperlukan.';
+$language['Please enter correct format in digit without -'] = 'Sila masukkan format yang betul dalam digit tanpa -';
+$language['Email is required'] = 'Emel diperlukan.';
+$language['Invalid email format'] = 'Format e-mel tidak sah.';
+
+$language['Changes failed. Name is required'] = 'Perubahan gagal. Nama diperlukan.';
+$language['Changes failed. Only letters and white space allowed'] = 'Perubahan gagal. Hanya huruf dan ruang putih yang dibenarkan.';
+$language['Changes failed. IC number is required'] = 'Perubahan gagal. Nombor IC diperlukan.';
+$language['Changes failed. Please enter 12 digit without - '] = 'Perubahan gagal. Sila masukkan 12 digit tanpa -';
+$language['Changes failed. Position is required.'] = 'Perubahan gagal. Jawatan diperlukan.';
+$language['Changes failed. Grade is required.'] = 'Perubahan gagal. Gred diperlukan.';
+$language['Changes failed. Address is required.'] = 'Perubahan gagal. Alamat Rumah diperlukan.';
+$language['Changes failed. Contact number is required.'] = 'Perubahan gagal. Telefon nombor diperlukan.';
+$language['Changes failed. Please enter correct format in digit without -'] = 'Perubahan gagal. Sila masukkan format yang betul dalam digit tanpa -';
+$language['Changes failed. Email is required'] = 'Perubahan gagal. Emel diperlukan.';
+$language['Changes failed. Invalid email format'] = 'Perubahan gagal. Format e-mel tidak sah.';
+$language['Are you sure you want to edit this account?'] = 'Adakah anda pasti mahu mengemaskinikna akaun ini?';
+
+//Grade
+$language['USER GRADE'] = 'GRED PENGGUNA';
+$language['Grade Settings'] = 'Tetapan Gred';
+$language['Grade ID:'] = 'Gred ID:';
+$language['Grade ID'] = 'Gred ID';
+$language['Enter Grade ID'] = 'Masukkan Gred ID';
+$language['Enter Position in English'] = 'Masukkan Jawatan dalam Bahasa Inggeris.';
+$language['Enter Position in Malay'] = 'Masukkan Jawatan dalam Bahasa Melayu.';
+$language['Grade List'] = 'Senarai Gred';
+$language['GRADE LIST'] = 'SENARAI GRED';
+$language['New Grade'] = 'Gred Baru';
+$language['Are you sure you want to delete this grade?'] = 'Adakah anda pasti mahu memadamkan gred ini?';
+$language['Do you want to save the changes?'] = 'Adakah anda mahu menyimpan perubahan ini?';
+$language['Grade ID is required'] = 'Gred ID diperlukan.';
+$language['Only letters, number and white space are allowed'] = 'Hanya huruf, nombor dan ruang putih sahaja yang dibenarkan';
+$language['Position in English is required'] = 'Jawatan dalam Bahasa Inggeris diperlukan.';
+$language['Only letters and white space allowed'] = 'Hanya huruf dan ruang putih yang dibenarkan.';
+$language['Position in Malay is required'] = 'Jawatan dalam Bahasa Melayu diperlukan.';
+$language['Changes failed. Grade ID is required'] = 'Perubahan gagal. Grade ID is required';
+$language['Changes failed. Only letters, number and white space are allowed']= 'Perubahan gagal. Only letters, number and white space are allowed';
+$language['Changes failed. Position in English is required'] = 'Perubahan gagal. Jawatan dalam Bahasa Inggeris diperlukan.';
+$language['Changes failed. Only letters and white space allowed']= 'Perubahan gagal. Hanya huruf dan ruang putih yang dibenarkan.';
+$language['Changes failed. Position in Malay is required'] = 'Perubahan gagal. Jawatan dalam Bahasa Melayu diperlukan.';
+$language['Changes failed. Only letters and white space allowed'] = 'Perubahan gagal. Hanya huruf dan ruang putih yang dibenarkan.';
+
 
 
 ?>
+
