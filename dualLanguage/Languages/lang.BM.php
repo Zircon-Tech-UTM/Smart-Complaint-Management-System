5f6aafbdff6396ca5db8cb09d5a7543edb564520
huiyou002013 <75981470+huiyou002013@users.noreply.github.com>
2021-01-19 14:41:00 +0800

    Update lang.BM.php

--- dualLanguage/Languages/lang.BM.php
+++ dualLanguage/Languages/lang.BM.php
@@ -1,12 +1,46 @@
 <?php
+//under comment "Dashboard, COMPLETED (same wif BI version)
+
 // Dashboard 
+$language['Number'] = 'Bilangan';
+$language["Assets's name"] = "Name Aset";
+$language['Number of Complaint'] = 'Bilangan Aduan';
+$language["Blocks' name"] = "Nama Blok";
+$language["Room's name"] = "Nama Bilik";
+$language['Status of non-ICT Complaints'] = 'Status Aduan untuk aset alih/ aset bukan ICT';
+$language['Status of ICT Complaints'] = 'Status Aduan untuk aset ICT';
+$language['colourcard1'] = 'ADUAN BARU SEPANJANG 7 HARI YANG LEPAS';
+$language['colourcard2'] = 'ADUAN SELESAI SEPANJANG 7 HARI YANG LEPAS';
+$language['colourcard3'] = 'JUMLAH ADUAN YANG BELUM SELESAI';
+$language['table'] = 'JUMLAH PENGGUNA DALAM SISTEM';
+$language['User'] = 'Pengguna';
+$language['bar1'] = 'ASET YANG MENERIMA PALING BANYAK ADUAN (10 YANG PALING TINGGI)';
+$language['bar2'] = 'BILIK YANG MENERIMA PALING BANYAK ADUAN  (5 YANG PALING TINGGI)';
+$language['bar3'] = 'BLOK YANG MENERIMA PALING BANYAK ADUAN  (3 YANG PALING TINGGI)';
+$language['pie1'] = 'CARTA ADUAN UNTUK ASET ICT';
+$language['pie2'] = 'CARTA ADUAN UNTUK ASET ALIH/ASET BUKAN ICT';
+$language['Complaint Status'] = 'Status Aduan';
+$language['KVPJB Complaint Inventory System'] = 'Sistem Inventori Aduan KVPJB'; 
+
 //$language[''] = '';
-$language['MY DASHBOARD'] = 'LAMAN UTAMA';
-$language['COMPLAINTS MANAGEMENT'] = 'SELENGARA ADUAN';
-$language['USER MANAGEMENT'] = 'SELENGARA PENGUNA';
-$language['ROOM MANAGEMENT'] = 'SELENGARA BILIK';
-$language['BLOCK MANAGEMENT'] = 'SELENGARA BLOK';
-$language['DOWNLOAD AND PRINT'] = 'MUAT TURUN DAN CETAK';
+
+//navbar1
+$language['Admin Home'] = '';
+$language['Admin Main Page'] = '';
+$language['ADMIN'] = '';
+
+$language['Edit Profile'] = 'Kemaskini Profil';
+$language['Change Password'] = 'Tukar Kata Laluan';
+$language['Logout'] = 'Log Keluar';
+$language['MANAGEMENT'] = 'LAMAN UTAMA';
+$language['MY DASHBOARD'] = 'PAPARAN SISTEM';
+$language['COMPLAINTS MANAGEMENT'] = 'ADUAN';
+$language['USER MANAGEMENT'] = 'PENGGUNA';
+$language['ROOM MANAGEMENT'] = 'BILIK';
+$language['BLOCK MANAGEMENT'] = 'BLOK';
+$language['ASSET MANAGEMENT'] = 'ASET';
+$language['DOWNLOAD AND PRINT'] = 'MUAT TURUN DAN CETAK DOCUMENT';
+$language['GRADE MANAGEMENT'] = 'GRED';
 
 //CRUD
 $language['Action'] = 'Tindakan';
@@ -67,7 +101,6 @@ $language['Manage Asset'] = 'Pengendalian Aset';
 $language['ICT'] = 'ICT';
 $language['Non-ICT'] = 'Bukan ICT';
 $language['Create Asset'] = 'Tambah Aset';
-
 
 
 
